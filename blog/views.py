b062cf6e27b03338ea64b9b1d33f97b6eba918c6
mustikatiara008 <76078749+mustikatiara008@users.noreply.github.com>
2022-10-27 21:54:15 +0700

    Add files via upload
    tugas

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 # VIEWS BLOG
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'blog/index.html')
+    data = Post.objects.all()
+    context = {
+        'title': 'Blog',
+        'heading': 'Blog',
+        'subheading': 'Postingan',
+        'post': data
+    }
+    return render(request, 'blog/index.html',context=context)
